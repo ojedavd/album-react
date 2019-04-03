58c287a9ae7492a2c5efb061e6b1f0bfa65f259e
victor <victor@MacBook.localdomain>
2019-04-03 17:47:53 -0300

    Scopes y permisos

--- src/Login.js
+++ src/Login.js
@@ -10,9 +10,11 @@ export default class Login extends Component{
 
     login(){
         let provider = new firebase.auth.GoogleAuthProvider();
-
+        provider.addScope('https://www.googleapis.com/auth/photoslibrary.readonly');
         firebase.auth().signInWithPopup(provider).then(result=>{
             console.log(result);
+        }).catch(err=>{
+            console.log(err);
         })
     }
 
