735566008309d8da827323a6857d51a5cce01122
victor <victor@MacBook.localdomain>
2019-04-03 18:12:19 -0300

    Barra de navegación con Material UI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withStyles } from '@material-ui/core/styles';
-import Login from './Login';
+
+import AppNav from './components/AppNav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header" className={this.props.classes.sombra}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <Login />
-        </header>
+          <AppNav />
       </div>
     );
   }
---
+++ src/components/AppNav.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Login from '../Login';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+class AppNav extends Component{
+    render(){
+        return(
+            <div>
+                <AppBar position='static'>
+                    <Toolbar>
+                        <Typography variant="h6" component="h1" className={this.props.classes.grow}>
+                            Albums
+                        </Typography>
+                        <Login />
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default withStyles({
+    grow:{
+      flexGrow:1,
+      textAlign:'left'
+    }
+  })(AppNav);
